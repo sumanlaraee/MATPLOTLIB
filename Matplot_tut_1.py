53494996962265907b9339de82c5dcf4aa3b3787
suman bai <127082344+sumanlaraee@users.noreply.github.com>
2023-08-28 13:53:53 -0700

    Add files via upload
    this is how to import matplot and draw a simple line or plot

---
+++ Matplot_tut_1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import matplotlib.pyplot as plt 
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[9]:
+
+
+x=[1,2, 3, 4, 5, 6]
+y=[50, 60, 20, 53, 45, 12]
+
+
+# In[21]:
+
+
+plt.xlabel("day")
+plt.ylabel("temperature")
+plt.title("weather graph")
+plt.plot(x, y , color="yellow", linewidth=12, linestyle="dotted")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
